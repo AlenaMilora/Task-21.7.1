873e38726632ec99cce653034cceb67e46e12e77
AlenaMilora <140298254+AlenaMilora@users.noreply.github.com>
2023-09-27 11:36:47 +0300

    Add files via upload

---
+++ testCat.py
@@ -0,0 +1,13 @@
+from Cat import Cat
+
+cat1 = Cat("Барон", "Мальчик", 2)
+
+print("Сat1.Имя", cat1.name)
+print("Сat1.Пол", cat1.gender)
+print("Сat1.Возраст", cat1.age)
+
+cat2 = Cat("Сэм", "Мальчик", 2)
+
+print("Cat2.Имя", cat2.name)
+print("Cat2.Пол", cat2.gender)
+print("Cat2.Возраст", cat2.age)
